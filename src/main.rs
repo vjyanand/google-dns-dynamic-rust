d64309b709c658e0111c544fea8e639fd6775ccd
Vijay <vijay@iavian.com>
2023-06-21 08:36:45 +0530

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
+use googlednsdynamic::gddns::{dns_lookup, my_ip, update_ip};
 use std::env;
-use std::io::{Error, ErrorKind};
-use std::net::{IpAddr, ToSocketAddrs};
-use std::str::FromStr;
 
 /// Updates dynamic ip managed by google domains
 ///
@@ -17,7 +15,7 @@ use std::str::FromStr;
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert_eq!(args.len(), 4, "Missing required parameters");
-    
+
     let host = &args[1];
     let user = &args[2];
     let password = &args[3];
@@ -46,39 +44,4 @@ fn main() {
     } else {
         println!("ip update failed")
     }
-}
-
-fn dns_lookup(domain: &str) -> Result<IpAddr, Error> {
-    let domain_with_ip = format!("{}:443", domain);
-    let addrs_iter = domain_with_ip.to_socket_addrs();
-    let mut addrs_iter = match addrs_iter {
-        Ok(addrs_iter) => addrs_iter,
-        Err(error) => return Err(error),
-    };
-    let socket_addr = addrs_iter.next();
-
-    let ip_addr = match socket_addr {
-        Some(ip) => ip.ip(),
-        None => return Err(Error::new(ErrorKind::Other, "oh no!")),
-    };
-    return Ok(ip_addr);
-}
-
-fn my_ip() -> Result<IpAddr, reqwest::Error> {
-    let resp = reqwest::blocking::get("https://api.ipify.org")?.text()?;
-    Ok(IpAddr::from_str(&resp).unwrap())
-}
-
-fn update_ip(hostname: &str, user: &str, passwd: &str, ip: IpAddr) -> Result<bool, reqwest::Error> {
-    let url = format!(
-        "https://{}:{}@domains.google.com/nic/update?hostname={}&myip={}",
-        user, passwd, hostname, ip
-    );
-    let client = reqwest::blocking::Client::new();
-    let resp = client.post(url).send().unwrap().text().unwrap();
-
-    if !(resp.starts_with("good") || resp.starts_with("nochg")) {
-        return Ok(false);
-    }
-    Ok(true)
 }
